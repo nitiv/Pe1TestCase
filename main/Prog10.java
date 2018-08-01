9370ba66befdea62c96a87ed538b0496b744e21b
nitiv <niti.vishwas@gmail.com>
2018-08-01 20:26:19 +0530

    first commit

---
+++ main/Prog10.java
@@ -0,0 +1,23 @@
+package com.stackroute.demo;
+
+//import java.util.Scanner;
+
+public class Prog10 
+{
+	public String method(String st) 
+	{
+		String s=st;
+		int n=3;
+			for(int i=0;i<n;i++)
+			{
+				for(int j=st.length()-n;j<st.length();j++)
+				{
+					s=s+(st.charAt(j));
+				}
+			}
+		return s;
+	
+	}			
+	
+}
+
---
+++ main/StringP.java
@@ -0,0 +1,17 @@
+package com.stackroute.demo;
+
+//import java.util.Scanner;
+
+public class StringP 
+{
+	public String reverse(String a)
+	{		
+		String str="";
+		char c[]=a.toCharArray();
+			for(int i=c.length-1;i>=0;i--) 
+			{
+				str=str+c[i];
+			}
+		return str;
+	}
+}
